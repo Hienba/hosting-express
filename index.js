3b40d6229ed9e2b83e8e57b302c70f32aad2f6ad
Hienba <96763849+Hienba@users.noreply.github.com>
2022-04-27 18:30:36 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('pages'));
+app.get("/", (req, res) => res.render("index.html"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
