0357fa3aea6bb73f3d228f26150f00aa6ee68ff9
Hienba <96763849+Hienba@users.noreply.github.com>
2022-04-28 09:52:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const index = require(".pages/index.html")
 const app = express();
 const port = 3000;
 // app.get("/", (req, res) => res.send("Hello World!"));
@@ -9,5 +8,4 @@ app.get("/trang-chu", (req,res) => res.redirect("https://google.com"));
 app.get("/", function (req, res) {
   res.render("index.html");
 });
-app.use("/index", index);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
