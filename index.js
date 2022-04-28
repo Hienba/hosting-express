21fbc74efbde3701e7fc8c0b0b51a73ba49bfc65
Hienba <96763849+Hienba@users.noreply.github.com>
2022-04-28 10:01:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 // app.get("/", (req, res) => res.send("Hello World!"));
 app.use(express.static("pages"));
+app.set('view engine', 'html')
 // app.get("/", (req, res) => res.json("data:success"));
 app.get("/trang-chu", (req,res) => res.redirect("https://google.com"));
 app.get("/", function (req, res) {
